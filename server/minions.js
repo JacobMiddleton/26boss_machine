f4e3dbb1a34fd5d290ba1d669ada44a25358fbdf
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2023-02-28 16:15:32 -0500

    Added routes for minions

--- server/minions.js
+++ server/minions.js
@@ -14,3 +14,31 @@ module.exports = minionsRouter;
 minionsRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('minions'));
 });
+
+minionsRouter.post('/', (req, res, next) => {
+    const minion = addToDatabase('minions', req.body);
+    res.status(201).send(minion);
+});
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    const minion = getFromDatabaseById('minions', req.params.minionId);
+    if (minion) {
+        res.send(minion);
+    } else {
+        res.status(204).send();
+    }
+});
+
+minionsRouter.put('/:minionId', (req, res, next) => {
+    const updatedMinion = updateInstanceInDatabase('minions', req.body);
+    res.send(updatedMinion);
+});
+
+minionsRouter.delete('/:minionId', (req, res, next) => {
+    const minion = deleteFromDatabasebyId('minions', req.params.minionId);
+    if (minion) {
+        res.status(204).send(minion);
+    } else {
+        res.status(500).send();
+    }
+});
