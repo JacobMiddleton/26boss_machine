18b131d2f7fe5a360b5ee6289dd785791ac8330b
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2023-02-28 16:53:55 -0500

    Added routes for ideas

--- server/ideas.js
+++ server/ideas.js
@@ -13,4 +13,32 @@ ideasRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('ideas'));
 });
 
+ideasRouter.post('/', (req, res, next) => {
+    const idea = addToDatabase('ideas', req.body);
+    res.status(201).send(idea);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+    const idea = getFromDatabaseById('ideas', req.params.ideaId);
+    if (idea) {
+        res.send(idea);
+    } else {
+        res.status(204).send();
+    }
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+    const updatedIdea = updateInstanceInDatabase('ideas', req.body);
+    res.send(updatedIdea);
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+    const idea = deleteFromDatabasebyId('ideas', req.params.ideaId);
+    if (idea) {
+        res.status(204).send();
+    } else {
+        res.status(500).send();
+    }
+});
+
 module.exports = ideasRouter;
